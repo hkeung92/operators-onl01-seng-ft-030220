6da3cb5c7139944b12ab97575220b921a413143b
Ho Yin Keung <hykeung92@gmail.com>
2020-03-14 02:24:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,10 @@
 def unsafe?(speed)
-  if speed > 60
+  if speed > 60 || speed < 40
     return true 
-  elsif speed < 40
-    return true
+  # elsif speed < 40
+  #   return true
   else false
+end
 end
 
 
